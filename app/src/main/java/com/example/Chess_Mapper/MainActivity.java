41b35f54f4bed1672155bbef16bfa35f36267995
ali-alshaar7 <alishaarawy2000@gmail.com>
2023-10-03 21:03:48 -0400

    third commit

--- app/src/main/java/com/example/Chess_Mapper/MainActivity.java
+++ app/src/main/java/com/example/Chess_Mapper/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.PPGAuth;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
+import android.content.Intent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -54,7 +55,8 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            return true;
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
         }
 
         return super.onOptionsItemSelected(item);
